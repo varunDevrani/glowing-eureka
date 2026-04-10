d09f2029610363852db24425de015bf07d5a65b7
varunDevrani <varun.devrani@intelegencia.com>
2026-04-10 12:25:58 +0530

    engine

--- src/database/connect.py
+++ src/database/connect.py
@@ -6,8 +6,13 @@ from sqlalchemy.orm import sessionmaker
 
 from src.core.config import settings
 
-
-engine = create_engine(settings.DATABASE_URL.get_secret_value())
+engine = create_engine(
+    settings.DATABASE_URL.get_secret_value(),
+    pool_pre_ping=True,
+    connect_args={
+        "prepare_threshold": 0
+    }
+)
 
 sessionLocal = sessionmaker(
 	bind=engine,
